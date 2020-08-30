5ddd00f024b93e2854b122e59a4c456bcf95ee4d
takechee <takechee14@gmail.com>
2020-08-30 22:01:03 +0900

    ランダム文字列ウィジェットを別クラス化

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:first_flutter_app/src/random_words.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,18 +20,5 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
   }
 }
